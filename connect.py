2717fe11fb66041a0ef67bbe0d5d1c0ba0c550a3
Jovian Hersemeule (ECN Lab) <jovian.hersemeule@eleves.ec-nantes.fr>
2019-04-03 09:10:41 +0200

    Updated credentials

--- connect.py
+++ connect.py
@@ -1,10 +1,10 @@
 import psycopg2
 
-conn = psycopg2.connect("host=myHost dbname=myDB user=xxx password=yyy")
+conn = psycopg2.connect("host=appli-pfe.ec-nantes.fr dbname=app_bdonn_01 user=app2_01 password=osiris")
 
 cursor = conn.cursor()
 
-cursor.execute("SELECT * from person")
+cursor.execute("SELECT * from Product")
 
 rows = cursor.fetchall()
 
