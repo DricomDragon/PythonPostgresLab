7af9b333e5c89450eec063d6870e4b85de931048
DricomDragon <contact@jovian-hersemeule.eu>
2019-09-23 10:47:16 +0200

    Change credentials file to use python_app user
    Avoi to use postgres user.

--- consumer/credentials.py
+++ consumer/credentials.py
@@ -1,3 +1,3 @@
 def getCredentials():
     """ Return credentials for the database """
-    return "host=localhost dbname=logistics user=postgres password=postgres_password"
+    return "host=localhost dbname=logistics user=python_app password=blablapoivron"
