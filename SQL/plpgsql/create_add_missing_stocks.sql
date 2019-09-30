7b81ac95a741ddb8ea5b64b9085dad79b4cf7b3b
DricomDragon <contact@jovian-hersemeule.eu>
2019-09-30 14:49:12 +0200

    Add the first loop iteration with a cursor

--- SQL/plpgsql/create_add_missing_stocks.sql
+++ SQL/plpgsql/create_add_missing_stocks.sql
@@ -1,13 +1,17 @@
 CREATE OR REPLACE FUNCTION add_missing_stocks() RETURNS void AS
 $$
 DECLARE
-    proCursor CURSOR IS SELECT pro_id FROM Product;
+    proCursor CURSOR FOR SELECT pro_id FROM Product;
     proId Product.pro_id%TYPE;
 BEGIN
     RAISE NOTICE 'Start';
-    FOR k IN 1..10 LOOP
-        RAISE NOTICE 'Loop %', k;
+    OPEN proCursor;
+    LOOP
+        FETCH proCursor INTO proId;
+        EXIT WHEN proId IS NULL;
+        RAISE NOTICE 'Loop %', proId;
     END LOOP;
+    CLOSE proCursor;
     RAISE NOTICE 'End';
 END;
 $$
