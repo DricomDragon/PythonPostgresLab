59df34194f503f1c8191ca072a9dffcbd45cbed9
DricomDragon <contact@jovian-hersemeule.eu>
2019-09-30 15:24:02 +0200

    Modify primary key to avoid storage duplication

--- SQL/plpgsql/create_stock_table.sql
+++ SQL/plpgsql/create_stock_table.sql
@@ -1,16 +1,10 @@
-CREATE SEQUENCE public.stock_sto_id_seq;
-
 CREATE TABLE public.Stock (
-                Sto_id INTEGER NOT NULL DEFAULT nextval('public.stock_sto_id_seq'),
                 Sto_quantity INTEGER NOT NULL DEFAULT 0,
                 Pro_id INTEGER NOT NULL,
                 War_id INTEGER NOT NULL,
-                CONSTRAINT stock_pk PRIMARY KEY (Sto_id)
+                CONSTRAINT stock_pk PRIMARY KEY (Pro_id, War_id)
 );
 COMMENT ON TABLE public.Stock IS 'Inventory of products.';
-
-
-ALTER SEQUENCE public.stock_sto_id_seq OWNED BY public.Stock.Sto_id;
 
 ALTER TABLE public.Stock ADD CONSTRAINT stock_product_fk
 FOREIGN KEY (Pro_id)
