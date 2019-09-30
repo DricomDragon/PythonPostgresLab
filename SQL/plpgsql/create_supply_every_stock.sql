c54dbdf1ad998495ae2b348bfce47d6d791b07b1
DricomDragon <contact@jovian-hersemeule.eu>
2019-09-30 16:02:28 +0200

    Create function to iterate over stock items

---
+++ SQL/plpgsql/create_supply_every_stock.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION supply_every_stock(amount INTEGER) RETURNS void AS
+$$
+DECLARE
+    stoCursor CURSOR IS SELECT sto_quantity FROM Stock FOR UPDATE;
+    stoQuantity Stock.sto_quantity%TYPE;
+BEGIN
+    RAISE NOTICE 'Iterate on existing stocks only';
+
+    -- Iterate on storage
+    OPEN stoCursor;
+    LOOP
+        FETCH stoCursor INTO stoQuantity;
+        EXIT WHEN stoQuantity IS NULL;
+
+        RAISE NOTICE 'Supply %', amount;
+    END LOOP;
+    CLOSE stoCursor;
+    RAISE NOTICE 'End';
+END;
+$$
+LANGUAGE plpgsql;
